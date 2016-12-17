19657a312ff0340740bb6f00fdcbf82e2ac3f383
Okan <okanbasar.metu@gmail.com>
2016-12-17 18:51:59 +0200

    Initial commit

---
+++ app/src/main/java/com/okanbasar/crashlyticsapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.okanbasar.crashlyticsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+    }
+}
